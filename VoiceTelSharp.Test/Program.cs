4de208d109f5701e46d70482dfdbb4632151d61e
Jonathan Rainier <pwnoz0r@gmail.com>
2018-02-27 13:06:05 -0500

    Project Setup
    + Added VoiceTelSharp.Lib
    + Added VoiceTelSharp.Test

---
+++ VoiceTelSharp.Test/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace VoiceTelSharp.Test
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
